871a077ecb495759d4d851b69deda2c4305d74cb
Jenifer Ananth <ananthfrancis>
2021-01-05 09:53:26 +0800

    updated confirmation model in requirements

--- src/pages/requirements/ViewRequirementAction.js
+++ src/pages/requirements/ViewRequirementAction.js
@@ -9,6 +9,7 @@ import Button from "../../common/forms/Button";
 import CustomDataTable from "../../common/CustomDataTable";
 import TableFilter from "../../common/TableFilter";
 import Col6 from "../../common/forms/Col6";
+
 // import requirementViewMore from "../../container/requirement/requirementViewMore";
 
 class ViewRequirementAction extends Component {
@@ -35,12 +36,46 @@ class ViewRequirementAction extends Component {
           />
         )}
         {/* <requirementViewMore showAddComponentModal={this.props.requirement.showrequirementMoreModal} /> */}
+        <ConfirmModal
+            showModal={this.state.showDeleteModal}
+            handleClose={() =>{
+              this.setState({ showDeleteModal: false, activeId: null })
+            }
+              
+            }
+            title="Approve Requirement"
+            handleConfirm={() => {
+              this.props.handleApprove(this.state.activeId);
+              this.setState({ showDeleteModal: false, activeId: null });
+            }}
+          >
+            <h6 className="text-danger">
+              Are you sure you want to Approve this request?
+            </h6>
+          </ConfirmModal>
+
+          <ConfirmModal
+            showModal={this.state.showDeleteModal}
+            handleClose={() =>{
+              this.setState({ showDeleteModal: false, activeId: null })
+            }
+            }
+            title="Reject Requirement"
+            handleConfirm={() => {
+              this.props.handleReject(this.state.activeId);
+              this.setState({ showDeleteModal: false, activeId: null });
+            }}
+          >
+            <h6 className="text-danger">
+              Are you sure you want to Reject this request?
+            </h6>
+          </ConfirmModal>
         <FormContainer formTitle={"View Requirements"}>
           {this.props.requirement.requirementsList && (
             <CustomDataTable
               metaData={listViewRequirementsMetaData(
-                (id) => this.props.handleApprove(id),
-                (id) => this.props.handleReject(id),
+                id => this.setState({ activeId: id, showDeleteModal: true }),
+                id => this.setState({ activeId: id, showDeleteModal: true }),
                 (id) => this.props.handleMore(id),
 
               )}
